d030be0d4bcd60a53e2882c3780aa9794c2678ba
21660 <2166054676@qq.com>
2026-09-18 12:16:15 +0800

    Initial commit: build git practice project skeleton

---
+++ src/main.py
@@ -0,0 +1,41 @@
+"""Git 练习项目的演示程序。
+
+刻意保持简单：这里只用一个 TodoList 来说明"代码会被反复修改"这件事，
+从而让 Git 的每一次提交都有实际内容可记录。
+"""
+
+
+class TodoList:
+    def __init__(self):
+        self.items = []
+
+    def add(self, title):
+        self.items.append({"title": title, "done": False})
+        return self.items[-1]
+
+    def finish(self, index):
+        self.items[index]["done"] = True
+
+    def pending(self):
+        return [it["title"] for it in self.items if not it["done"]]
+
+
+def main():
+    todo = TodoList()
+    todo.add("初始化 Git 仓库")
+    todo.add("配置远程仓库")
+    todo.add("完成分支实验")
+
+    todo.finish(0)
+
+    print("Git Practice Project")
+    print("-" * 24)
+    for i, item in enumerate(todo.items, 1):
+        mark = "x" if item["done"] else " "
+        print("[%s] %d. %s" % (mark, i, item["title"]))
+    print("-" * 24)
+    print("未完成：%d 项" % len(todo.pending()))
+
+
+if __name__ == "__main__":
+    main()
